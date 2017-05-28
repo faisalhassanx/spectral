0db5a22feafeba4d463509ea83795a2b609f5ff7
Faisal Hassan <faisal.hassanx@gmail.com>
2017-05-28 00:10:10 +0000

    Generate chefs table and add chef and recipe models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170527235217) do
+ActiveRecord::Schema.define(version: 20170528000547) do
+
+  create_table "chefs", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
