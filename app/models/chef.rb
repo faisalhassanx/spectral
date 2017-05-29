ef9a763267bd3c388660cb0a23480ab0132e8689
Faisal Hassan <faisal.hassanx@gmail.com>
2017-05-29 19:46:59 +0000

    Add tests and validations for chef name and email

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,3 +1,7 @@
 class Chef < ActiveRecord::Base
-  
+  validates :name,  presence: true, length: { in: 3..40 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 105 },
+                    uniqueness: { case_sensitive: false },
+                    format:     { with: VALID_EMAIL_REGEX }
 end
