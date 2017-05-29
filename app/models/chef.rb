23a94de52fd0f2199aac84bf020da59c1502783b
Faisal Hassan <faisal.hassanx@gmail.com>
2017-05-29 19:52:10 +0000

    Add before save filter to chef model to downcase emails before saving

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,4 +1,5 @@
 class Chef < ActiveRecord::Base
+  before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { in: 3..40 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 105 },
