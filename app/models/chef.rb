19f67b2378b21abac60cbb81f78c78cc497dce55
Faisal Hassan <faisal.hassanx@gmail.com>
2017-06-02 22:48:10 +0000

    Create like model and add associations between likes and chefs and recipes

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,5 +1,6 @@
 class Chef < ActiveRecord::Base
   has_many :recipes
+  has_many :likes
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { in: 3..40 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
