6e3c4cad04695773120cdce3c12aeb705dcb61ce
Faisal Hassan <faisal.hassanx@gmail.com>
2017-06-02 03:45:02 +0000

    Add form field for picture upload and whitelist picture params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -50,7 +50,7 @@ class RecipesController < ApplicationController
   private
   
   def recipe_params
-    params.require(:recipe).permit(:name, :summary, :description)
+    params.require(:recipe).permit(:name, :summary, :description, :picture)
   end
   
 end
