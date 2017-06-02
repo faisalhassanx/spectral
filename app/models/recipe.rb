161a39e01b29c5ac736bcf9bafeff80d1f46c057
Faisal Hassan <faisal.hassanx@gmail.com>
2017-06-02 04:21:31 +0000

    Add picture size validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,4 +8,13 @@ class Recipe < ActiveRecord::Base
   validates :description, presence: true,
                           length: { in: 20..1000 }
   mount_uploader :picture, PictureUploader
+  validate :picture_size
+  
+  private
+  
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 end
