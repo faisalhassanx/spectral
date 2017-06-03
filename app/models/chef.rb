269c336ab25e1e560a222adf89954e1c52e4403b
Faisal Hassan <faisal.hassanx@gmail.com>
2017-06-03 19:42:11 +0000

    Add likes validation

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,6 +1,6 @@
 class Chef < ActiveRecord::Base
-  has_many :recipes
-  has_many :likes
+  has_many :recipes, dependent: :destroy
+  has_many :likes, dependent: :destroy
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { in: 3..40 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
