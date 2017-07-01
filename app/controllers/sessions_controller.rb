c681600afaa9a4ff8f669f15f88e9c1a03862bb5
Faisal Hassan <faisal.hassanx@gmail.com>
2017-07-01 00:23:51 +0000

    Add create and destroy session actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,21 @@ class SessionsController < ApplicationController
   end
   
   def create
-    
+    chef = Chef.find_by(email: params[:email])
+    if chef && chef.authenticate(params[:password])
+      session[:chef_id] = chef.id
+      flash[:success] = "You have logged in successfully"
+      redirect_to recipes_path
+    else
+      flash.now[:danger] = "Invalid email or password"
+      render new
+    end
   end
   
   def destroy
-    
+    session[:chef_id] = nil
+    flash[:success] = "You have logged out successfully"
+    redirect_to root_path
   end
   
 end
