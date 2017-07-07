785009ebc76f8088144821f727278349e85fa404
Faisal Hassan <faisal.hassanx@gmail.com>
2017-07-07 23:58:47 +0000

    Generate migration and create table for ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170707235514) do
+ActiveRecord::Schema.define(version: 20170707235748) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 20170707235514) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "likes", force: :cascade do |t|
