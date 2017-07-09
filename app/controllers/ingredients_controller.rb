540eeed97723328ef5cf51d4d421d52995be2190
Faisal Hassan <faisal.hassanx@gmail.com>
2017-07-09 23:28:25 +0000

    Add new ingredient form partial and methods for whitelisting params

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -5,11 +5,23 @@ class IngredientsController < ApplicationController
   end
   
   def new
-    
+    @ingredient = Ingredient.new
   end
   
   def create
-    
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      flash[:success] = "Ingredient has been saved successfully"
+      redirect_to recipes_path
+    else
+      flash[:danger] = "Please try again"
+      render 'new'
+    end
   end
   
+  private
+  
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
 end
