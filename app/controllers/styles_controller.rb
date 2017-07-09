194cf943684b3291656953f8b119efc8d9b3a984
Faisal Hassan <faisal.hassanx@gmail.com>
2017-07-09 22:47:16 +0000

    Add create method and whitelist style params for name

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -8,7 +8,22 @@ class StylesController < ApplicationController
   end
   
   def create
+    @style = Style.new(style_params)
+    if @style.save
+      flash[:success] = "Style created successfully"
+      redirect_to recipes_path
+    else
+      flash[:danger] = "Please try again"
+      render 'new'
+    end
+      
     
+  end
+  
+  private
+  
+  def style_params
+    params.require(:style).permit(:name)
   end
   
 end
