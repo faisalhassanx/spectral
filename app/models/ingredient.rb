3cb7872525314d26001e11cbf365d1f7d8ba3e31
Faisal Hassan <faisal.hassanx@gmail.com>
2017-07-12 22:25:46 +0000

    Remove dependent destroy from styles and ingredients models

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,7 +2,7 @@ class Ingredient < ActiveRecord::Base
   validates :name,  presence: true,
                     length: { in: 2..25 }
   has_many :recipe_ingredients
-  has_many :recipes, through: :recipe_ingredients, dependent: :destroy
+  has_many :recipes, through: :recipe_ingredients
 
   
 end
--- app/models/style.rb
+++ app/models/style.rb
@@ -2,6 +2,6 @@ class Style < ActiveRecord::Base
   validates :name,  presence: true,
                     length: { in: 2..25 }
   has_many :recipe_styles
-  has_many :recipes, through: :recipe_styles, dependent: :destroy
+  has_many :recipes, through: :recipe_styles
   
 end
