ff01ebb23029c7d7f59c3c7a54a2f54111df759e
Faisal Hassan <faisal.hassanx@gmail.com>
2017-07-12 20:43:09 +0000

    Add admin functionality to show view for editing recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -64,7 +64,7 @@ class RecipesController < ApplicationController
   end
   
   def require_same_user
-    if current_user != @recipe.chef
+    if current_user != @recipe.chef and !current_user.admin?
       flash[:danger] = "You can only edit your own recipes"
       redirect_to recipes_path
     end
